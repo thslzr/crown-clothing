3cb37d96bb21eaf430f0017d24b88fd739f0f2a3
Victor Salazar <victor_s_m@hotmail.es>
2021-09-20 23:17:55 -0500

    Update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './menu-item.styles.scss'
+import { withRouter  } from 'react-router-dom';
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+    console.log(match.url)
     return (
-        <div className={ `${ size } menu-item` }>
+        <div className={ `${ size } menu-item` } onClick={ () => history.push(`${ match.url }${ linkUrl }`) }>
             <div className='background-image' style={{
                 backgroundImage: `url(${ imageUrl })`
             }} />
@@ -15,4 +17,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
     )
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
